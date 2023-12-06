0a859d5372abd8f13aef29f7ed98bd2af79653b8
Lin Jack <linjack@192.168.0.31>
2023-12-06 19:10:26 +0800

    js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,6 +1,8 @@
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
+    
+    var p = defaultPatient();
 
     function onError() {
       console.log('Loading error', arguments);
@@ -9,8 +11,8 @@
 
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
         var pt = patient.read();
+        var patient = smart.patient;
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
@@ -42,7 +44,6 @@
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
-          var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
@@ -71,6 +72,51 @@
     return ret.promise();
 
   };
+
+  $(document).ready(function() {
+    // 监听表单提交事件
+    $('#updateForm').on('submit', function(event) {
+        event.preventDefault(); // 阻止表单默认提交行为
+
+        // 获取用户输入
+        var updatedFname = $('#updatedFname').val();
+        var updatedLname = $('#updatedLname').val();
+
+        // 构建需要更新的患者信息对象
+        p.name = [{
+          use: "official",
+          family: [updatedLname],
+          given: [updatedFname]
+        }];
+
+        // 发送更新请求到FHIR服务器
+        updatePatientData(p);
+    });
+  });
+
+  function updatePatientData(patientData) {
+    // 假设 `smart` 是全局可用的SMART on FHIR对象
+    var client = FHIR.client({
+      serviceUrl: smart.server.serviceUrl,
+      patientId: patientData.id
+    });
+  
+    // 构造更新请求
+    var updateRequest = {
+      type: 'Patient',
+      id: patientData.id,
+      resource: patientData
+    };
+  
+    // 发送更新请求
+    client.update(updateRequest).then(function(response) {
+      // 处理成功的响应
+      console.log('Patient updated: ', response);
+    }).catch(function(error) {
+      // 处理错误
+      console.error('Failed to update patient: ', error);
+    });
+  }
 
   function defaultPatient(){
     return {
